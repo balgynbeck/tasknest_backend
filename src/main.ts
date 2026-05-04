adfcf1a567b0ec60d9710c1a77b210665cacc333
balgynbeck <bek.balgynbeks@gmail.com>
2026-05-04 15:34:37 +0500

    Fix CORS for production

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,22 +10,22 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
-    .setTitle('Task Management API')
-    .setDescription('API для работы с досками, задачами и пользователями')
-    .setVersion('1.0')
-    .addTag('Boards', 'Эндпоинты для работы с досками')
-    .addTag('Tasks', 'Эндпоинты для работы с задачами')
-    .addTag('Users', 'Эндпоинты для работы с пользователями')
-    .addTag('Auth', 'Аутентификация и авторизация')
+    .setTitle("Task Management API")
+    .setDescription("API для работы с досками, задачами и пользователями")
+    .setVersion("1.0")
+    .addTag("Boards", "Эндпоинты для работы с досками")
+    .addTag("Tasks", "Эндпоинты для работы с задачами")
+    .addTag("Users", "Эндпоинты для работы с пользователями")
+    .addTag("Auth", "Аутентификация и авторизация")
     .addBearerAuth()
-    .addCookieAuth('refreshToken')
+    .addCookieAuth("refreshToken")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || "http://localhost:3001",
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
